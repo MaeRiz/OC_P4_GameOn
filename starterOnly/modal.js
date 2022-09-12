989b88a9216a79637a9edad36606d4acb881063d
Mahery Gonin <mahery.gonin@gmail.com>
2022-09-12 10:42:23 +0200

    add check fields

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalForm = document.querySelector("#modal-form")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -24,3 +25,118 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// Check form
+
+modalForm.email.addEventListener('change', function() {
+  checkField('email', this)
+});
+
+modalForm.first.addEventListener('change', function() {
+  checkField('name', this)
+});
+
+modalForm.last.addEventListener('change', function() {
+  checkField('name', this)
+});
+
+modalForm.birthdate.addEventListener('change', function() {
+  checkField('date', this)
+});
+
+modalForm.quantity.addEventListener('change', function() {
+  checkField('num', this)
+});
+
+const checkField = function(type, data) {
+  let emailRegEx = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$', 'g');
+  let nameRegEx = new RegExp("^[a-z ,.'-]{2,}$" ,'i');
+  let dateRegEx = new RegExp('^\\d{4}-\\d{2}-\\d{2}$', 'g');
+  let tournaCountRegEx = new RegExp('[0-9]$')
+
+  let small = data.nextElementSibling;
+
+  if (type == 'email') {
+
+    if (!emailRegEx.test(data.value)){
+      small.innerHTML = 'Veuillez entrer une adresse email valide !';
+      return false;
+    }
+    else {
+      small.innerHTML = '';
+      return true;
+    };
+  }
+
+  if (type == 'name') {
+
+    if (!nameRegEx.test(data.value)){
+      small.innerHTML = 'Veuillez entrer un nom de plus de 2 carractères !';
+      return false
+    }
+    else {
+      small.innerHTML = '';
+      return true
+    };
+  }
+
+  if (type == 'date') {
+
+    if (!dateRegEx.test(data.value)){
+      small.innerHTML = 'Vous devez entrer une date de naissance valide !';
+      return false
+    }
+    else {
+      small.innerHTML = '';
+      return true
+    };
+  }
+
+  if (type == 'num') {
+
+    if (!tournaCountRegEx.test(data.value)){
+      small.innerHTML = 'Vous devez indiquer un chiffre !';
+      return false
+    }
+    else {
+      small.innerHTML = '';
+      return true
+    };
+  }
+
+};
+
+const checkRadio = function(){
+  const radioButtons = document.querySelectorAll('input[name="location"]');
+  let small = document.querySelector("#radio-location");
+  for (const radioButton of radioButtons) {
+    if (radioButton.checked) {
+      small.innerHTML = '';
+      return true;
+    }
+    else {
+      small.innerHTML = 'Vous devez choisir une ville !';
+    }
+  }
+}
+
+const checkCheckBox = function() {
+  const checkBoxBtn = document.querySelector('input[id="checkbox1"]');
+  let small = document.querySelector("#error-checkbox");
+
+  if (checkBoxBtn.checked) {
+    small.innerHTML = '';
+    return true;
+  }
+  else {
+    small.innerHTML = 'Vous devez accepter les CGU !';
+  };
+
+};
+
+modalForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  if (checkField('name', modalForm.first) && checkField('name', modalForm.last) && checkField('email', modalForm.email) && checkField('date', modalForm.birthdate) && checkField('num', modalForm.quantity) && checkRadio() && checkCheckBox()) {
+    modalForm.submit();
+  };
+});
